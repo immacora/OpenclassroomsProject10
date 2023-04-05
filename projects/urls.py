f7a54df5bb78426546f7b8092b54883f5116b4fa
Coralie <immacora@gmail.com>
2023-04-05 15:11:00 +0200

    Reset

--- projects/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'projects'
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name="projects/index.html")),
-]
