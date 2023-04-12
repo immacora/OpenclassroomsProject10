1f033705ffccfd827ef5b0bd4e6f9dad87faed69
Coralie <immacora@gmail.com>
2023-04-12 17:36:05 +0200

    Ajout de la fonction isalphavalidator

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,14 @@
 import uuid
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from helpers.models import TrackingModel
+from helpers.validators import isalphavalidator
 from django.db import models
 from django.contrib.auth.models import (PermissionsMixin, AbstractBaseUser)
 
 from .managers import CustomUserManager
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin, TrackingModel):
+class CustomUser(PermissionsMixin, AbstractBaseUser):
     """Utilisateur personnalisé."""
 
     user_id = models.UUIDField(
@@ -18,8 +18,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin, TrackingModel):
     )
     email = models.EmailField(_("email address"), blank=False, unique=True)
     username = None
-    first_name = models.CharField(_("first name"), max_length=150, blank=False, null=False)
-    last_name = models.CharField(_("last name"), max_length=150, blank=False, null=False)
+    first_name = models.CharField(_("first name"), validators=[isalphavalidator], max_length=150, blank=False, null=False)
+    last_name = models.CharField(_("last name"), validators=[isalphavalidator], max_length=150, blank=False, null=False)
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
@@ -37,13 +37,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin, TrackingModel):
 
     objects = CustomUserManager()
 
-    EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
-    @property
-    def token(self):
-        return ''
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
