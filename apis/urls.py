fc62f0055a1ff812a81ab36464eb78269f635e70
Coralie <immacora@gmail.com>
2023-04-21 12:31:37 +0200

    Ajout du validateur isalphanumvalidator

--- apis/urls.py
+++ apis/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 
-from .views import SignupAPIView, ProjectListAPIView, ProjectDetailAPIView
+from .views import (
+    SignupAPIView,
+    ProjectListAPIView,
+    ProjectDetailAPIView,
+    ContributorsAPIView,
+    ContributorDeleteAPIView
+)
 
 urlpatterns = [
     path('signup/', SignupAPIView.as_view(), name='signup'),
@@ -9,5 +15,11 @@ urlpatterns = [
     path('logout/', TokenBlacklistView.as_view(), name='logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('projects/', ProjectListAPIView.as_view(), name='projects'),
-    path('projects/<uuid:pk>/', ProjectDetailAPIView.as_view(), name='projects_detail'),
+    path('projects/<uuid:pk>/', ProjectDetailAPIView.as_view(), name='project_detail'),
+    path('projects/<uuid:pk>/users/', ContributorsAPIView.as_view(), name='contributors'),
+    path('projects/<uuid:pk>/users/<uuid:pk>', ContributorDeleteAPIView.as_view(), name='delete_contributor'),
+    #projects/<uuid:pk>/issues/
+    #projects/<uuid:pk>/issues/<uuid:pk>
+    #projects/<uuid:pk>/issues/<uuid:pk>/comments/
+    #projects/<uuid:pk>/issues/<uuid:pk>/comments/<uuid:pk>
 ]
--- helpers/validators.py
+++ helpers/validators.py
@@ -6,3 +6,10 @@ isalphavalidator = RegexValidator(
     message="La saisie doit comporter uniquement des lettres avec trait d'union ou espace pour séparation.",
     code='Saisie invalide'
     )
+
+
+isalphanumvalidator = RegexValidator(
+    r'^[0-9A-Za-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ._\s-]*$',
+    message="La saisie doit comporter uniquement des lettres avec trait d'union ou espace pour séparation et des chiffres.",
+    code='Saisie invalide'
+    )
